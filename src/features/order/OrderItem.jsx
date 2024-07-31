9813215648b7708a6771269d0a8cb0f262492d73
SubajananiSwaminathan <subajanani@gmail.com>
2024-07-31 12:06:57 +0530

    Styled using Tailwind CSS

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,13 +1,15 @@
+import { formatCurrency } from "../../utilities/helpers";
+
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li>
+    <li className="py-3">
       <div>
-        <p>
-          <span>{quantity}&times;</span> {name}
+        <p className="flex items-center justify-between gap-4 text-sm">
+          <span className="font-bold">{quantity}&times;</span> {name}
         </p>
-        <p>{formatCurrency(totalPrice)}</p>
+        <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
     </li>
   );
